88ac8ad18ad9e7ac8b0f74797492a33463c54af0
Stijn Soete <stijn@phronesys.eu>
2022-01-10 15:52:33 +0100

    provide index of rating to matches

--- lib/src/models/rating.dart
+++ lib/src/models/rating.dart
@@ -1,11 +1,14 @@
 /// Dice's Coefficient result
 class Rating {
-  Rating({this.target, this.rating});
+  Rating({this.target, this.rating, required this.index});
 
   /// reference text
   String? target;
+
   /// between 0 and 1. 0 indicates completely different strings, 1 indicates identical strings.
   double? rating;
+
+  int index;
 
   @override
   String toString() => '\'$target\'[$rating]';
